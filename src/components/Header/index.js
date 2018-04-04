a5517fddbd11646a1a82f9090429f312fa7479d6
Ash Sykes <ash.sykes@edgetic.com>
2018-04-04 17:06:48 +0100

    Update header bar
    - Update header to contain desired content
    - TODO: ensure menu opens when mobile display is active

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,36 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import { Container, Tabs, TabList, Tab, TabLink } from 'bloomer'
+import { Container, Tabs, TabList, Tab, TabLink, Navbar, NavbarBrand, NavbarMenu, NavbarEnd, NavbarItem, NavbarBurger, Icon } from 'bloomer'
+
+const brand = require('../../assets/logo.jpg');
 
 const AppHeader = () => (
-  <Container>
-    <Tabs isAlign="centered">
-      <TabList>
-        <Tab>
-          <TabLink>This is always at the top</TabLink>
-        </Tab>
-        <Tab>
-          <Link to="/">Home</Link>
-        </Tab>
-        <Tab>
-          <Link to="/page-2/">Go to page 2</Link>
-        </Tab>
-      </TabList>
-    </Tabs>
-  </Container>
+  <Navbar>
+    <Container>
+      <NavbarBrand>
+        <NavbarItem>
+          <img src={brand} style={{ marginRight: 5 }} /> <Link to="/">Sykomaniac</Link>
+        </NavbarItem>
+        <NavbarBurger />
+      </NavbarBrand>
+      <NavbarMenu>
+        <NavbarEnd>
+          <NavbarItem isHidden='touch'>
+            <Link to="/page-2/">My CV</Link>
+          </NavbarItem>
+          <NavbarItem href="https://github.com/Sykomaniac" target="_blank" isHidden='touch'>
+              <Icon className='fa fa-github' />
+          </NavbarItem>
+          <NavbarItem href="https://plus.google.com/+AshleySykes" target="_blank" isHidden='touch'>
+              <Icon className='fa fa-google-plus' style={{ color: '#d34836' }} />
+          </NavbarItem>
+          <NavbarItem href="https://twitter.com/ashsykes15" target="_blank" isHidden='touch'>
+              <Icon className='fa fa-twitter' style={{ color: '#55acee' }} />
+          </NavbarItem>
+        </NavbarEnd>
+      </NavbarMenu>
+    </Container>
+  </Navbar>
 )
 
 export default AppHeader
